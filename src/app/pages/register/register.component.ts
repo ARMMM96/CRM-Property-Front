89eb4b9af4f76a40c34dddb544946021c056c68a
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-29 06:45:38 +0200

    Rgister completed

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -8,14 +8,58 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  isSubmit: boolean = false
+  errorFlag = false
+  errorMsg = ''
+
+
 
   registerForm = new FormGroup({
+    firstName: new FormControl("", [Validators.required, Validators.min(3)]),
+    lastName: new FormControl("", [Validators.required, Validators.min(3)]),
+    age: new FormControl("", [Validators.required, Validators.min(2)]),
+    phoneNum: new FormControl("", [Validators.required, Validators.min(11)]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.min(6)]),
+    repeatedPassword: new FormControl("", [Validators.required, Validators.min(6)]),
+    gender: new FormControl("", [Validators.required]),
 
   })
   constructor(private user: UserService, private router: Router) { }
 
   handleRegister() {
     console.log("fire?")
+    console.log(this.registerForm.value.password)
+    this.isSubmit = true
+    if (this.registerForm.value.password != this.registerForm.value.repeatedPassword) {
+      this.errorFlag = true
+      this.errorMsg = 'Passowrd and repeat password must be the same'
+
+    } else {
+      this.errorFlag = false
+      this.errorMsg = ''
+    }
+
+    if (this.registerForm.valid) {
+      this.user.retisterUser(this.registerForm.value).subscribe(
+        {
+          next: (res) => {
+            complete: () => this.router.navigateByUrl('/login')
+          },
+          error: (e) => {
+            console.log(e.error.message)
+            this.errorFlag = true
+            this.errorMsg = e.error.message
+          }
+        }
+      )
+    }
+
+
+
+
+
+
   }
 
 }
