a960205dcb6e2b261fef40a2182addb244ba1095
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-29 05:26:31 +0200

    Navbar component updated
    Navbar updated to include the new routes and the methods that render the user data on the nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  userName: any
+  userImage: any
+
+  constructor(protected user: UserService) {
+    if (localStorage.getItem('token')) {
+      this.user.isLogIn = true
+      this.user.userInfo().subscribe({
+        next: (res) => {
+          console.log(res.data.user)
+          this.userName = res.data.user.firstName
+          if (res.data.user.image) {
+            console.log(`${user.mainUrl}/pictures/${res.data.user.image.slice(16)}`)
+            this.userImage = `${user.mainUrl}/pictures/${res.data.user.image.slice(16)}`
+          } else {
+            this.userImage = false
+          }
+        }
+      })
+    }
+    console.log(this.user.isLogIn)
+  }
+
+
+
+  logOut() {
+    localStorage.removeItem('token')
+  }
 
 }
