b1700163f7a7a522098d8088cdfdd674eeb7d30a
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-29 09:52:36 +0200

    Card component implemented
    Card component updated to render the values by input

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  @Input() project:any
 
 }
