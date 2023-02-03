15ee08bb87d7d04f5721510a57828169f4664613
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-02-03 08:39:40 +0200

    Is logged out guard added

---
+++ src/app/guards/is-logged-out.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsLoggedOutGuard implements CanActivate {
+  constructor(private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let token = localStorage.getItem('token')
+    if (token) {
+      this.router.navigateByUrl('home')
+      return false;
+    }
+
+    return true
+  }
+
+}
