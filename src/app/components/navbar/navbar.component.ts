5afab1000c4827104dd0d33a6f7e2b736c012cad
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-02-03 09:54:16 +0200

    Navbar component updated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -15,39 +15,60 @@ export class NavbarComponent {
   showDashBoredLink: boolean = false
 
   constructor(protected user: UserService) {
-    if (localStorage.getItem('token')) {
-      this.user.isLogIn = true
-      this.user.userInfo().subscribe({
-        next: (res) => {
-          if (res.data.user.role) {
-            this.userRoleId = res.data.user.role
-            this.user.getUserRole(this.userRoleId).subscribe({
-              next: (res) => {
-                this.roleType = res.data.roleType
-                console.log(res.data.roleType)
-              }
-            })
 
-          }
-          if(this.roleType =='Owner' || 'admin'){
-            this.showDashBoredLink = true
-          }
-          this.userName = res.data.user.firstName
-          if (res.data.user.image) {
-            console.log(`${user.mainUrl}/pictures/${res.data.user.image.slice(16)}`)
-            this.userImage = `${user.mainUrl}/pictures/${res.data.user.image.slice(16)}`
-          } else {
-            this.userImage = false
-          }
-        }
-      })
+  }
+  ngOnInit(): void {
+    if (localStorage.getItem('token')) {
+      this.rednerUserData()
     }
     console.log(this.user.isLogIn)
   }
+  rednerUserData() {
+    this.user.isLogIn = true
+
+  }
+
+  getUserInfo() {
+    this.user.userInfo().subscribe({
+      next: (res) => {
+        if (res.data.user.role) {
+          this.userRoleId = res.data.user.role
+          this.user.getUserRole(this.userRoleId).subscribe({
+            next: (res) => {
+              this.roleType = res.data.roleType
+              console.log(res.data.roleType)
+            }
+          })
+
+        }
+
+        this.userName = res.data.user.firstName
+        if (res.data.user.image) {
+          console.log(`${this.user.mainUrl}/pictures/${res.data.user.image.slice(16)}`)
+          this.userImage = `${this.user.mainUrl}/pictures/${res.data.user.image.slice(16)}`
+        } else {
+          this.userImage = false
+        }
+      }
+    })
+  }
+
+
+  toggleDashboredRouteLink() {
+    if (this.roleType == 'Owner' || 'admin') {
+      this.showDashBoredLink = true
+    }
+  }
+
+
+
+
+
 
 
   logOut() {
     localStorage.removeItem('token')
+    localStorage.removeItem('role')
   }
 
 }
