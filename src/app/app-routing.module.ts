d273c6339597c2e633eb9cfe469569d831ff15c8
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-02-03 08:44:18 +0200

    App Routing Module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsLoggedInGuard } from './guards/is-logged-in.guard';
+import { IsLoggedOutGuard } from './guards/is-logged-out.guard';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UnitsComponent } from './pages/units/units.component';
 import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, canActivate: [IsLoggedOutGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'units', component: UnitsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'profile', component: UserComponent },
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'profile', component: UserComponent, canActivate: [IsLoggedInGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [IsLoggedInGuard] },
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
