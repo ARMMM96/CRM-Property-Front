2df51980700290b1625099f24b8c4764df4bbc34
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2023-02-04 23:58:02 +0200

    Register form validation implemented
    Validation and error messages added to the registration form inputs

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -13,24 +13,37 @@ export class RegisterComponent {
   errorMsg = ''
 
 
+  constructor(private user: UserService, private router: Router) { }
 
   registerForm = new FormGroup({
-    firstName: new FormControl("", [Validators.required, Validators.min(3)]),
-    lastName: new FormControl("", [Validators.required, Validators.min(3)]),
-    age: new FormControl("", [Validators.required, Validators.min(2)]),
-    phoneNum: new FormControl("", [Validators.required, Validators.min(11)]),
-    email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.required, Validators.min(6)]),
-    repeatedPassword: new FormControl("", [Validators.required, Validators.min(6)]),
+    firstName: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(10), Validators.pattern('^[_A-z0-9]*((-|s)*[_A-z0-9])*$')]),
+    lastName: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(10), Validators.pattern('^[_A-z0-9]*((-|s)*[_A-z0-9])*$')]),
+    age: new FormControl("", [Validators.required, Validators.min(18), Validators.max(60)]),
+    phoneNum: new FormControl("", [Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern("^01[0125][0-9]{8}$")]),
+    email: new FormControl("", [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/)]),
+    repeatedPassword: new FormControl("", [Validators.required]),
     gender: new FormControl("", [Validators.required]),
 
   })
-  constructor(private user: UserService, private router: Router) { }
+
+
+  get firstName() { return this.registerForm.get('firstName') }
+  get lastName() { return this.registerForm.get('lastName') }
+  get age() { return this.registerForm.get('age') }
+  get phoneNum() { return this.registerForm.get('phoneNum') }
+  get email() { return this.registerForm.get('email') }
+  get password() { return this.registerForm.get('password') }
+  get repeatedPassword() { return this.registerForm.get('repeatedPassword') }
+  get gender() { return this.registerForm.get('gender') }
+
 
   handleRegister() {
     console.log("fire?")
-    console.log(this.registerForm.value.password)
+
     this.isSubmit = true
+
+    console.log(this.registerForm.value.password)
     if (this.registerForm.value.password != this.registerForm.value.repeatedPassword) {
       this.errorFlag = true
       this.errorMsg = 'Passowrd and repeat password must be the same'
@@ -39,7 +52,7 @@ export class RegisterComponent {
       this.errorFlag = false
       this.errorMsg = ''
     }
-
+    console.log(this.registerForm.errors)
     if (this.registerForm.valid) {
       this.user.retisterUser(this.registerForm.value).subscribe(
         {
@@ -50,11 +63,11 @@ export class RegisterComponent {
             console.log(e.error.message)
             this.errorFlag = true
             this.errorMsg = e.error.message
-          }
+          },
+          complete: () => this.router.navigateByUrl('/login')
         }
       )
     }
-
 
 
 
